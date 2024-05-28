656633911acd6967f06163feeb31222173749636
Your Name <dwijjoshi02@gmail.com>
2024-05-29 00:08:54 +0530

    Added create and join room feature

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -2,11 +2,12 @@ import { WebSocket } from "ws";
 import { Game } from "./Game";
 
 export class GameManager {
-  private games: Game[];
+  private games: any;
   private pendingUser: WebSocket | null;
   private users: WebSocket[];
+
   constructor() {
-    this.games = [];
+    this.games = {};
     this.pendingUser = null;
     this.users = [];
   }
@@ -26,24 +27,50 @@ export class GameManager {
     socket.on("message", (data) => {
       const message = JSON.parse(data.toString());
 
-      if (message.type === "init_game") {
-        if (this.pendingUser) {
-          const game = new Game(this.pendingUser, socket);
-          this.games.push(game);
-          this.pendingUser = null;
+      // if (message.type === "init_game") {
+      //   if (this.pendingUser) {
+      //     const game = new Game(this.pendingUser, socket);
+      //     this.games.push(game);
+      //     this.pendingUser = null;
+      //   } else {
+      //     this.pendingUser = socket;
+      //   }
+      // }
+      if (message.type === "create") {
+        const code = "dwij2";
+        this.games[code] = {
+          code,
+          player1: socket,
+          player2: null,
+        };
+        socket.send(JSON.stringify({ type: "created", code }));
+      }
+
+      if (message.type === "join") {
+        const code = message.code;
+        const game = this.games[code];
+        if (game && !game.player2) {
+          game.player2 = socket;
+          game.player1!.send(JSON.stringify({ type: "joined", code }));
+          game.player2.send(JSON.stringify({ type: "joined", code }));
         } else {
-          this.pendingUser = socket;
+          socket.send(
+            JSON.stringify({
+              type: "error",
+              message: "Invalid code or game already full",
+            })
+          );
         }
       }
 
-      if (message.type === "move") {
-        const game = this.games.find(
-          (game) => game.player1 === socket || game.player2 === socket
-        );
-        if (game) {
-          game.makeMove(socket, message.move);
-        }
-      }
+      // if (message.type === "move") {
+      //   const game = this.games.find(
+      //     (game) => game.player1 === socket || game.player2 === socket
+      //   );
+      //   if (game) {
+      //     game.makeMove(socket, message.move);
+      //   }
+      // }
     });
   }
 }
